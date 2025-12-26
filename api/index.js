db2ac45af769eafcfa061c36bd1ac52b7ed86d6b
Rahel Moges <mogesrahel430@gmail.com>
2025-12-26 13:36:39 +0300

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
 
 dotenv.config();
 
@@ -15,6 +16,10 @@ mongoose
     console.log(err);
   });
 
-app.listen(3000, () => {
+app.listen(30001, () => {
   console.log('Server is running on port 30001!');
 });
+
+
+app.use("/api/user", userRouter);
+
