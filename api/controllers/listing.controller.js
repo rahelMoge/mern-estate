a709ef20821fa522cdfa73529cad987f77b52e9f
Rahel Moges <mogesrahel430@gmail.com>
2026-04-08 14:25:06 +0300

    create get user listings api

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,10 +1,36 @@
 import Listing from "../models/listing.model.js";
 
-export const createListing = async (req, res, next) => {
-    try {
-        const listing = await Listing.create(req.body);
-        return res.status(201).json(listing);
-    } catch (error) {
-        next(error);
+// Controller object
+const listingController = {
+    // Create a new listing
+    createListing: async (req, res, next) => {
+        try {
+            const listing = await Listing.create(req.body);
+            return res.status(201).json(listing);
+        } catch (error) {
+            next(error);
+        }
+    },
+
+    // Get listings for a specific user
+    getUserListings: async (req, res, next) => {
+        try {
+            const listings = await Listing.find({ userRef: req.params.id });
+            return res.status(200).json(listings);
+        } catch (error) {
+            next(error);
+        }
+    },
+
+    // Optionally, get all listings
+    getAllListings: async (req, res, next) => {
+        try {
+            const listings = await Listing.find();
+            return res.status(200).json(listings);
+        } catch (error) {
+            next(error);
+        }
     }
-}
+};
+
+export default listingController;
