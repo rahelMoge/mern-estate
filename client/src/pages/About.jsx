720aee893681a58baa975d56880fbbd085e8c496
Rahel Moges <mogesrahel430@gmail.com>
2026-04-22 14:13:19 +0300

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <div>
+        <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Rahel Estate</h1>
+        <p className='mb-4 text-slate-700'>Rahel Estate is a real estate company that specializes in buying, selling, and renting properties.         <p className='mb-4 text-slate-700'>Our team of experienced professionals is dedicated to providing our clients with the best possible service.
+          We believe that everyone deserves to have a place to call home, and we are committed to helping our clients find the perfect property for their needs. </p>
+        </p>
+        <p className='mb-4 text-slate-700'>Whether you are looking to buy, sell, or rent a property, Rahel Estate has something for everyone. We have a wide range of properties to choose from, including houses, apartments, and commercial properties. We also offer a range of services to help you find the perfect property for your needs.</p>
+        <p className='mb-4 text-slate-700'>Contact us today to learn more about our properties and services. Our team is always happy to help you find the perfect property for your needs.</p>
+      </div>
+    </div>
   )
 }
